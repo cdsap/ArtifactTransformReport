c0b76151b15a526b99c87d3dfb0412c78e196d25
Iñaki Villar <inaki.seri@gmail.com>
2026-08-29 02:36:05 +0200

    fix: refactor report output orchestration to a shared writer (#92)

--- cli/src/main/java/io/github/cdsap/artifacttransform/cli/report/ArtifactTransformReport.kt
+++ cli/src/main/java/io/github/cdsap/artifacttransform/cli/report/ArtifactTransformReport.kt
@@ -11,7 +11,7 @@ class ArtifactTransformReport(
 
     suspend fun process() {
         val transforms = GetArtifactTransforms(filter, repository).get()
-        ArtifactTransformReportOutputs(transforms, false) {
+        ReportOutputWriter(transforms, false) {
             println("Total Artifact transforms: ${transforms.size}")
             println("Build Scans with Artifact transforms: ${transforms.groupBy { it.buildScanId }.count()}")
         }.write()
